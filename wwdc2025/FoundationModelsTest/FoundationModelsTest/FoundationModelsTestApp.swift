0d12fb9e01283368ba4c8b5fcc5251dfd5419bcd
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2025-06-16 20:24:09 +0900

    [add] FoundationModelsEx

---
+++ wwdc2025/FoundationModelsTest/FoundationModelsTest/FoundationModelsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsTestApp.swift
+//  FoundationModelsTest
+//
+//  Created by SHIRAISHI HIROYUKI on 2025/06/15.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
