6ad0b31e68cef7cf8077280ac47d33de5f1c7965
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2025-07-21 21:47:39 +0900

    [add] FoundationModels FirsComit

---
+++ wwdc2025/FoundationModelsTest/FoundationModelsTest/GradientGenerator.swift
@@ -0,0 +1,20 @@
+//
+//  GradientGenerator.swift
+//  FoundationModelsTest
+//
+//  Created by SHIRAISHI HIROYUKI on 2025/06/26.
+//
+
+import SwiftUI
+
+struct GradientGenerator: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    GradientGenerator()
+}
